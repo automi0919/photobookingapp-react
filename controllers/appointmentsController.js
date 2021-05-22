58cb626d449ef6a998abc189e75eda6c21093750
Michael Knowlton <mknowlton89@gmail.com>
2021-05-21 21:07:00 -0500

    Figured out how to pass data in to the get request. I had to use req.query as opposed to req.body or req.params.

--- controllers/appointmentsController.js
+++ controllers/appointmentsController.js
@@ -7,8 +7,7 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   getAppointmentsByDate: function (req, res) {
-    db.Appointment.find({ date: '2021-05-23' })
-      .then(console.log(req.body))
+    db.Appointment.find( req.query )
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
