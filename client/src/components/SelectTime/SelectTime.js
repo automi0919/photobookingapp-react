dcc5e6757b42d97540090dd28845568ce73f4c25
Michael Knowlton <mknowlton89@gmail.com>
2021-05-22 19:43:38 -0500

    Got the state for the existing appointments to set and I'm currently mapping over the available appointments and rendering those on the page. Now I need to figure out how to disable the radio button IF they appointment is already booked.

--- client/src/components/SelectTime/SelectTime.js
+++ client/src/components/SelectTime/SelectTime.js
@@ -1,22 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import API from '../../utils/API';
 
-// const availability = ['9:00 - 10:30', '11:00 - 12:30', '1:00 - 2:30', '3:00 - 4:30', '5:00 - 6:30'];
-let appointments = [];
-let availabilityList = [];
 
-// let availabilityList = availability.filter((slot) => !appointments.includes(slot));
 
 export function SelectTime({ appointmentData, setAppointmentData }) {
 
+    const [appointments, setAppointments] = useState()
+    const availability = ['9:00 - 10:30', '11:00 - 12:30', '1:00 - 2:30', '3:00 - 4:30', '5:00 - 6:30'];
+    let availabilityList = [];
     let history = useHistory();
 
-    function showAvailableSlots() {
-        const availability = ['9:00 - 10:30', '11:00 - 12:30', '1:00 - 2:30', '3:00 - 4:30', '5:00 - 6:30'];
-
-        return availabilityList = availability.filter((slot) => !appointments.includes(slot));
-    }
+    // function showAvailableSlots() {
+    //     // console.log(appointments);
+    //     const availabilityList = availability.filter(slot => appointments.date !== slot);
+    // }
 
     function handleChange(field, value) {
         setAppointmentData(prevState => {
@@ -33,20 +31,53 @@ export function SelectTime({ appointmentData, setAppointmentData }) {
 
     function getAppointments() {
         API.getAppointments(appointmentData)
-            .then((res) => appointments = res.data)
-            .then(console.log(appointments))
-            .then(showAvailableSlots);
+            .then((res) => setAppointments(res.data))
     };
+
+
 
     useEffect(() => {
         getAppointments();
     }, []);
 
+    // showAvailableSlots();
+
     return (
         <div>
             <h1>Select a Time Slot</h1>
             <div className="choose-time-button-container">
-                {availabilityList.map((slot) =>
+                {/* {!appointments ? <div>Loading...</div> :
+                    availabilityList.map((slot) =>
+                    (
+                        <>
+                            <input
+                                type="radio"
+                                id={slot}
+                                value={slot}
+                                name="appointmentSelection"
+                                onChange={(e) => handleChange("timeSlot", e.target.value)}
+                            />
+                            <span>{slot}</span>
+                        </>
+                    )
+                    )
+                } */}
+
+                {appointments ? availability.map((slot) => (
+                    <>
+                        <input
+                            type="radio"
+                            id={slot}
+                            value={slot}
+                            name="appointmentSelection"
+                            onChange={(e) => handleChange("timeSlot", e.target.value)}
+                        />
+                        <span>{slot}</span>
+                    </>
+                )) : <div className="table-loading">Loading Appointment Data</div>}
+
+
+                {/* {availabilityList.map((slot) =>
                 (
                     <>
                         <input
@@ -59,7 +90,8 @@ export function SelectTime({ appointmentData, setAppointmentData }) {
                         <span>{slot}</span>
                     </>
                 )
-                )}
+                )} */}
+
             </div>
             <button onClick={handleSubmit}>Book Appointment</button>
         </div>
