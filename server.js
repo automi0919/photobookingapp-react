33a3f0b86b415ecabcfa1903d04bcc8188d8a5ee
Michael Knowlton <mknowlton89@gmail.com>
2021-05-30 20:17:22 -0400

    deploying

--- server.js
+++ server.js
@@ -16,9 +16,17 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/photoschedule");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/photoschedule",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
